1216c87272c1fc095681e037570254af91d05cf4
anastasiiastarostenko <anastasiia@mppteam.com>
2022-04-29 20:41:11 +0300

    Add assosiations between models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  has_many :questions, dependent: :destroy
+  has_many :test_results, dependent: :nullify
+  has_many :users, through: :test_results
+
+  belongs_to :category
+  belongs_to :author, class_name: 'User', inverse_of: :authored_tests
+
   class << self
     def by_category(category_name)
       joins('JOIN categories ON tests.category_id = categories.id')
