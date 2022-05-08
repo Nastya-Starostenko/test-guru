7460b721690f8b6207087057b29817be0b0512c4
anastasiiastarostenko <anastasiia@mppteam.com>
2022-05-08 10:16:08 +0300

    Add-validation-and-scopes-to-models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,15 @@
 
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :body, presence: true
+  validate :validate_answers_count_in_test, on: :create
+
+  scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_answers_count_in_test
+    errors.add(:answers, 'must be between 1 and 4') if question.answers.count >= 4
+  end
 end
