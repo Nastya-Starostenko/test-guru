54824ac14766d144124d53d13e7183b2cc1ab3fb
anastasiiastarostenko <anastasiia@mppteam.com>
2022-05-20 07:40:14 +0300

    Add-views-for-questions-and-helpers

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module QuestionHelper
+  def question_header(question)
+    test_title = question.test.title
+    question.persisted? ? "Edit <#{test_title}> Question" : "Create New <#{test_title}> Question"
+  end
 end
