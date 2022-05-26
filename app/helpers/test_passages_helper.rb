81aad446834972c181e3129c5cf477ea80779e1c
anastasiiastarostenko <anastasiia@mppteam.com>
2022-05-26 16:33:33 +0300

    Add-test-passages

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,18 +1,8 @@
 # frozen_string_literal: true
 
 module TestPassagesHelper
-  def get_title(test_passage)
-    base_string = "The #{test_passage.test.title} test was "
-    "#{base_string}#{result_in_word(test_passage)} completed!"
-  end
-
-  def get_title_class(test_passage)
-    "#{result_in_word(test_passage)}_title"
-  end
-
-  private
-
-  def result_in_word(test_passage)
-    test_passage.result_in_percent >= 85 ? 'successful' : 'failed'
+  def get_result_title(test_passage)
+    base_string = "The #{test_passage.test.title} test was"
+    "#{base_string} #{test_passage.successful? ? 'successful completed!' : 'failed!'}"
   end
 end
